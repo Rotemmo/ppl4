be8a8b4e972c6d003fb57b1c5febd5f1c2db38cb
Ran Brachel <ranbrachel@Rans-MacBook-Pro.local>
2025-06-17 21:02:50 +0300

    5.2

--- ex4.pl
+++ ex4.pl
@@ -11,6 +11,7 @@
  * You can enlarge it, if needed.
  * **********************************************
  */
+
 maximum_printing_depth(100).
 :- current_prolog_flag(toplevel_print_options, A),
    (select(max_depth(_), A, B), ! ; A = B),
@@ -37,28 +38,42 @@ book(the_well_at_the_worlds_end, m, f, s(s(s(s(zero))))).
 book(the_hobbit, t, f, s(s(s(zero)))).
 book(the_lord_of_the_rings, t, f, s(s(s(s(s(s(zero))))))).
 
-% You can add more facts.
+% Helper to compare Church numerals
+greater_than(zero, zero) :- false.
+greater_than(s(_), zero).
+greater_than(s(X), s(Y)) :- greater_than(X, Y).
 
+% max_list(List, Max) implementation
+max_list([], false).
+max_list([X], X).
+max_list([X|Xs], Max) :-
+    max_list(Xs, TempMax),
+    (greater_than(X, TempMax) -> Max = X ; Max = TempMax).
 
-% Signature: max_list(Lst, Max)/2
-% Purpose: true if Max is the maximum church number in Lst, false if Lst is emoty.
+% author_of_genre(GenreName, AuthorName)
+author_of_genre(GenreName, AuthorName) :-
+    genre(GenreId, GenreName),
+    author(AuthorId, AuthorName),
+    book(_, AuthorId, GenreId, _).
 
+% longest_book(AuthorName, BookName)
+longest_book(AuthorName, BookName) :-
+    author(AuthorId, AuthorName),
+    findall((Length, Name),
+            book(Name, AuthorId, _, Length),
+            Pairs),
+    extract_lengths(Pairs, Lengths),
+    max_list(Lengths, Max),
+    member((Max, BookName), Pairs).
 
+extract_lengths([], []).
+extract_lengths([(Len, _)|Rest], [Len|Others]) :-
+    extract_lengths(Rest, Others).
 
+/** <examples>
 
+?- max_list([s(zero), s(s(zero)), zero], Max).
+?- author_of_genre(fantasy, Author).
+?- longest_book(tolkien, Book).
 
-
-
-
-% Signature: author_of_genre(GenreName, AuthorName)/2
-% Purpose: true if an author by the name AuthorName has written a book belonging to the genre named GenreName.
-
-
-
-
-
-
-
-
-% Signature: longest_book(AuthorName, BookName)/2
-% Purpose: true if the longest book that an author by the name AuthorName has written is titled BookName.
+*/
